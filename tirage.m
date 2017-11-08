b8fae04af6293cfb4cf1def4af82868ee77f9fa5
GillesCrutzen <33466394+GillesCrutzen@users.noreply.github.com>
2017-11-08 23:06:53 +0100

    Update tirage.m

--- tirage.m
+++ tirage.m
@@ -1,7 +1,7 @@
 function [ E ] = tirage(n, Data)
 
     E = zeros(n, 4);
-    Rand_vec = randi([0,100], n, 1 );
+    Rand_vec = randi([1,100], n, 1 );
     for i=1:n
         E(i,:) = Data(Rand_vec(i),:);
     end
