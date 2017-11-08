234ad9258352a65a61f703f5ed865daad1958395
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-08 22:50:26 +0100

    Update tirage.m

--- tirage.m
+++ tirage.m
@@ -1,9 +1,9 @@
-function [ E ] = tirage(n, M)
+function [ E ] = tirage(n, Data)
 
     E = zeros(n, 4);
     Rand_vec = randi([0,100], n, 1 );
     for i=1:n
-        E(i,:) = M(randi(i),:);
+        E(i,:) = Data(Rand_vec(i),:);
     end
 end
 
