dbe8fbf842a0c3f854680c8af806e8fe375e215d
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-08 23:17:30 +0100

    Update tirage.m

--- tirage.m
+++ tirage.m
@@ -1,3 +1,13 @@
+%==========================================================================
+% Cette fonction tire un échantillon i.i.d. de taille n dans la population
+% Data.
+%
+% ARGUMENTS:
+%       -> n : taille de l'échantillon
+%       -> Data : données de la population
+% RETURN:
+%       -> E : données de l'échantillon
+%==========================================================================
 function [ E ] = tirage(n, Data)
 
     E = zeros(n, 4);
