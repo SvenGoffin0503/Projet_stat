e6e4f2b4ff169e4c4aa015d67749c6e032087192
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-16 17:09:07 +0100

    Update Q2a.m

--- Q2a.m
+++ Q2a.m
@@ -4,7 +4,7 @@
 %   GOFFIN Sven
 %   CRUTZEN Gilles
 %==========================================================================
-%% Chargement des données
+%% Chargement des donnees
 
 Data = csvread('db_stat75.csv',1,1);
 Size = size(Data);
@@ -16,11 +16,11 @@ if(Size(1) ~= 100 || Size(2) ~= 4)
     disp('ERREUR : LECTURE DU FICHIER ERRONEE');
 end
 %--------------------------------------------------------------------------
-%% (a) Tirage de l'echantillon
+%% 2(a) Tirage de l'echantillon
 
-E = tirage(n, Data);
+E = tirage(n, Data, 0);
 %--------------------------------------------------------------------------
-%% (a)i. Moyenne, mediane et ecart-type de l'echantillon (biere et spiritueux)
+%% 2(a)i. Moyenne, mediane et ecart-type de l'echantillon (biere et spiritueux)
 
 Moy_E_beer = mean(E(:,1));
 Moy_E_spir = mean(E(:,2));
@@ -31,7 +31,7 @@ Med_E_spir = median(E(:,2));
 ET_E_beer = std(E(:,1));
 ET_E_spir = std(E(:,2));
 %--------------------------------------------------------------------------
-%% (a)ii. Boites a moustaches (consommation de biere et de spiritueux)
+%% 2(a)ii. Boites a moustaches (consommation de biere et de spiritueux)
 
 E_red = zeros(n,2);
 E_red(:,1) = E(:,1);
@@ -39,7 +39,7 @@ E_red(:,2) = E(:,2);
 
 boxplot(E_red);
 %--------------------------------------------------------------------------
-%% (a)iii. Polygones des frequences cumulees de la consommation de biere et de spiritueux
+%% 2(a)iii. Polygones des frequences cumulees de la consommation de biere et de spiritueux
 
 Cons_E_beer = 0:max(Data(:,1));
 Cons_E_spir = 0:max(Data(:,2));
@@ -51,7 +51,7 @@ for i = 1:n
     Freq_E_beer(1, E(i, 1) + 1) = Freq_E_beer(1, E(i, 1) + 1) + 1;
     Freq_E_spir(1, E(i, 2) + 1) = Freq_E_spir(1, E(i, 2) + 1) + 1;
 end
-
+figure;
 Freq_E_beer = Freq_E_beer / n;
 Freq_E_spir = Freq_E_spir / n;
 Freq_E_cum_beer = cumsum(Freq_E_beer);
@@ -76,11 +76,9 @@ end
 
 Freq_beer = Freq_beer / N;
 Freq_spir = Freq_spir / N;
-Freq_cum_bier = cumsum(Freq_beer);
-Freq_cum_fort = cumsum(Freq_spir);
+Freq_cum_beer = cumsum(Freq_beer);
+Freq_cum_spir = cumsum(Freq_spir);
 
-Dist_beer = abs(Freq_cum_bier - Freq_E_cum_beer);
-Dist_spir = abs(Freq_cum_fort - Freq_E_cum_spir);
-Dist_KS_beer = max(Dist_beer);
-Dist_KS_spir = max(Dist_spir);
+[h,p,ks_beer] = kstest2(Freq_cum_beer, Freq_E_cum_beer);
+[h,p,ks_spir] = kstest2(Freq_cum_spir, Freq_E_cum_spir);
 %--------------------------------------------------------------------------
