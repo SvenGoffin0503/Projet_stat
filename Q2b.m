d6efb70a3e2493923deb1e6fbfa4610e90b7d6cc
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-25 11:14:36 +0100

    Update Q2b.m

--- Q2b.m
+++ Q2b.m
@@ -1,8 +1,8 @@
 %==========================================================================
-% Question 2 : Generation d'echantillon i.i.d.
+% Question 2 : Generation d'echantillons i.i.d.
 %   
-%   GOFFIN Sven
 %   CRUTZEN Gilles
+%   GOFFIN Sven
 %==========================================================================
 %% Chargement des donnees
 
@@ -16,155 +16,165 @@ if(Size(1) ~= 100 || Size(2) ~= 4)
     disp('ERREUR : LECTURE DU FICHIER ERRONEE');
 end
 %--------------------------------------------------------------------------
-%% 2(b)i  Moyenne
+%% 2(b) i. , 2(b) ii. , 2(b) iii.
 
-vect_E = zeros(100,n,4);
-
-for i = 1:100
-    vect_E(i,:,:) = datasample(Data,n,1);
-end
-
-Vec_moy_E_beer = zeros(100,1);
-Vec_moy_E_spir = zeros(100,1);
-Vec_med_E_beer = zeros(100,1);
-Vec_med_E_spir = zeros(100,1);
-Vec_ET_E_beer = zeros(100,1);
-Vec_ET_E_spir = zeros(100,1);
+Vect_ech = zeros(100,n,4);
 
 for i = 1:100
-    Vec_moy_E_beer(i) = mean(vect_E(i,:,1));
-    Vec_moy_E_spir(i) = mean(vect_E(i,:,2));
-    
-    Vec_med_E_beer(i) = median(vect_E(i,:,1));
-    Vec_med_E_spir(i) = median(vect_E(i,:,2));
-    
-    Vec_ET_E_beer(i) = std(vect_E(i,:,1));
-    Vec_ET_E_spir(i) = std(vect_E(i,:,2));
+    Vect_ech(i,:,:) = datasample(Data,n,1);
 end
 
-Vec_moy_E_beer;
-Vec_moy_E_spir;
+Vec_moy_ech_beer = zeros(100,1);
+Vec_moy_ech_spir = zeros(100,1);
+Vec_med_ech_beer = zeros(100,1);
+Vec_med_ech_spir = zeros(100,1);
+Vec_ET_ech_beer = zeros(100,1);
+Vec_ET_ech_spir = zeros(100,1);
 
+for i = 1:100
+    Vec_moy_ech_beer(i) = mean(Vect_ech(i,:,1));
+    Vec_moy_ech_spir(i) = mean(Vect_ech(i,:,2));
+    
+    Vec_med_ech_beer(i) = median(Vect_ech(i,:,1));
+    Vec_med_ech_spir(i) = median(Vect_ech(i,:,2));
+    
+    Vec_ET_ech_beer(i) = std(Vect_ech(i,:,1));
+    Vec_ET_ech_spir(i) = std(Vect_ech(i,:,2));
+end
+
+Vec_moy_ech_beer;
+Vec_moy_ech_spir;
 %--------------------------------------------------------------------------
 %% figures
 
+% Histogramme des moyennes des conso. de biere des echantillons
 figure
-histogram(Vec_moy_E_beer,'FaceColor','b');
-%title('histograme des moy des conso des bieres');
+histogram(Vec_moy_ech_beer,'FaceColor','b');
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
+
+% Histogramme des moyennes des conso. d'alcool fort des echantillons
 hold on
-histogram(Vec_moy_E_spir,'FaceColor','y');
-%title('histograme des moy des conso des fort');
+histogram(Vec_moy_ech_spir,'FaceColor','y');
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
 
 xlabel('Moyennes (canettes-shots)');
 ylabel('Frequences correspondantes');
 
-Moy_vec_moy_bier=mean(Vec_moy_E_beer);
-Moy_vec_moy_fort=mean(Vec_moy_E_spir);
+% Histogramme des medianes des conso. de biere des echantillons
+figure;
+histogram(Vec_med_ech_beer,'FaceColor','b');
+set(gca, 'fontsize', 20);
+set(gcf,'color','w');
 
-figure
-histogram(Vec_med_E_beer,'FaceColor','b');
-%title('histograme des med des conso des bieres');
-set(gca, 'fontsize', 20);
-set(gcf,'color','w');
+% Histogramme des medianes des conso. d'alcool fort des echantillons
 hold on
-histogram(Vec_med_E_spir,'FaceColor','y');
-%title('histograme des med des conso des fort');
+histogram(Vec_med_ech_spir,'FaceColor','y');
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
+
 xlabel('Medianes (canettes-shots)');
 ylabel('Frequences correspondantes ');
 
-Moy_vec_med_bier=mean(Vec_med_E_beer);
-Moy_vec_med_fort=mean(Vec_med_E_spir);
+% Histogramme des ET des conso. de biere des echantillons
+figure;
+histogram(Vec_ET_ech_beer,'FaceColor','b');
+set(gca, 'fontsize', 20);
+set(gcf,'color','w');
 
-figure
-histogram(Vec_ET_E_beer,'FaceColor','b');
-%title('histograme des ET des conso des bieres');
-set(gca, 'fontsize', 20);
-set(gcf,'color','w');
+% Histogramme des ET des conso. d'alcool fort des echantillons
 hold on
-histogram(Vec_ET_E_spir,'FaceColor','y');
-%title('histograme des ET des conso des fort');
+histogram(Vec_ET_ech_spir,'FaceColor','y');
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
+
 xlabel('Ecart-types (canettes-shots)');
 ylabel('Frequences correspondantes');
 
-Moy_vec_ET_bier=mean(Vec_ET_E_beer);
-Moy_vec_ET_fort=mean(Vec_ET_E_spir);
+%--------------------------------------------------------------------------
+%% Calcul des moyennes des differents vecteurs
 
-%% Calcul des distances de Kolmogorov Smirnov
+Moy_vec_moy_beer = mean(Vec_moy_ech_beer);
+Moy_vec_moy_spir = mean(Vec_moy_ech_spir);
+
+Moy_vec_med_beer = mean(Vec_med_ech_beer);
+Moy_vec_med_spir = mean(Vec_med_ech_spir);
+
+Moy_vec_ET_beer = mean(Vec_ET_ech_beer);
+Moy_vec_ET_spir = mean(Vec_ET_ech_spir);
+%--------------------------------------------------------------------------
+%% 2(b) iv. , 2(b) v.
 
 % Calcul des frequences cumulees population
-
-Freq_beer_popu = zeros(1, max(Data(:,1)) + 1);
-Freq_spir_popu = zeros(1, max(Data(:,2)) + 1);
-Freq_wine_popu = zeros(1, max(Data(:,3)) + 1);
-Freq_pure_popu = zeros(1, 10 * max(Data(:,4)) + 1);
+Freq_beer_pop = zeros(1, max(Data(:,1)) + 1);
+Freq_spir_pop = zeros(1, max(Data(:,2)) + 1);
+Freq_vin_pop = zeros(1, max(Data(:,3)) + 1);
+Freq_pur_pop = zeros(1, 10 * max(Data(:,4)) + 1);
 
 for i = 1:N
-    Freq_beer_popu(1, Data(i, 1) + 1) = Freq_beer_popu(1, Data(i, 1) + 1) + 1;
-    Freq_spir_popu(1, Data(i, 2) + 1) = Freq_spir_popu(1, Data(i, 2) + 1) + 1;
-    Freq_wine_popu(1, Data(i, 3) + 1) = Freq_wine_popu(1, Data(i, 3) + 1) + 1;
-    Freq_pure_popu(1, 10 * Data(i, 4) + 1) = Freq_pure_popu(1, 10 * Data(i, 4) + 1) + 1;
+    Freq_beer_pop(1, Data(i,1) + 1) = Freq_beer_pop(1, Data(i,1) + 1) + 1;
+    Freq_spir_pop(1, Data(i,2) + 1) = Freq_spir_pop(1, Data(i,2) + 1) + 1;
+    Freq_vin_pop(1, Data(i, 3) + 1) = Freq_vin_pop(1, Data(i, 3) + 1) + 1;
+    Freq_pur_pop(1, 10 * Data(i,4) + 1) = Freq_pur_pop(1, 10 * Data(i,4)...
+        + 1) + 1;
 end
 
-Freq_beer_popu = Freq_beer_popu / N;
-Freq_spir_popu = Freq_spir_popu / N;
-Freq_wine_popu = Freq_wine_popu / N;
-Freq_pure_popu = Freq_pure_popu / N;
+Freq_beer_pop = Freq_beer_pop / N;
+Freq_spir_pop = Freq_spir_pop / N;
+Freq_vin_pop = Freq_vin_pop / N;
+Freq_pur_pop = Freq_pur_pop / N;
 
-Freq_cum_beer_popu = cumsum(Freq_beer_popu);
-Freq_cum_spir_popu = cumsum(Freq_spir_popu);
-Freq_cum_wine_popu = cumsum(Freq_wine_popu);
-Freq_cum_pure_popu = cumsum(Freq_pure_popu);
+Freq_cum_beer_pop = cumsum(Freq_beer_pop);
+Freq_cum_spir_pop = cumsum(Freq_spir_pop);
+Freq_cum_vin_pop = cumsum(Freq_vin_pop);
+Freq_cum_pur_pop = cumsum(Freq_pur_pop);
 
 % Calcul des frequences cumulees echantillons
-
-Freq_E_beer = zeros(100, max(Data(:,1)) + 1);
-Freq_E_spir = zeros(100, max(Data(:,2)) + 1);
-Freq_E_wine = zeros(100, max(Data(:,3)) + 1);
-Freq_E_pure = zeros(100, 10 * max(Data(:,4)) + 1);
+Freq_ech_beer = zeros(100, max(Data(:,1)) + 1);
+Freq_ech_spir = zeros(100, max(Data(:,2)) + 1);
+Freq_ech_vin = zeros(100, max(Data(:,3)) + 1);
+Freq_ech_pur = zeros(100, 10 * max(Data(:,4)) + 1);
 
 for i = 1:100
     for j = 1:n
-        Freq_E_beer(i, vect_E(i, j, 1) + 1) = Freq_E_beer(i, vect_E(i, j, 1) + 1) + 1;
-        Freq_E_spir(i, vect_E(i, j, 2) + 1) = Freq_E_spir(i, vect_E(i, j, 2) + 1) + 1;
-        Freq_E_wine(i, vect_E(i, j, 3) + 1) = Freq_E_wine(i, vect_E(i, j, 3) + 1) + 1;
-        Freq_E_pure(i, 10 * vect_E(i, j, 4) + 1) = Freq_E_pure(i, 10 * vect_E(i, j, 4) + 1) + 1;
+        Freq_ech_beer(i, Vect_ech(i, j, 1) + 1) = ...
+                        Freq_ech_beer(i, Vect_ech(i, j, 1) + 1) + 1;
+        Freq_ech_spir(i, Vect_ech(i, j, 2) + 1) = ...
+                        Freq_ech_spir(i, Vect_ech(i, j, 2) + 1) + 1;
+        Freq_ech_vin(i, Vect_ech(i, j, 3) + 1) = ...
+                        Freq_ech_vin(i, Vect_ech(i, j, 3) + 1) + 1;
+        Freq_ech_pur(i, 10 * Vect_ech(i, j, 4) + 1) = ...
+                        Freq_ech_pur(i, 10 * Vect_ech(i, j, 4) + 1) + 1;
     end
 end
 
-Freq_E_beer = Freq_E_beer / n;
-Freq_E_spir = Freq_E_spir / n;
-Freq_E_wine = Freq_E_wine / n;
-Freq_E_pure = Freq_E_pure / n;
+Freq_ech_beer = Freq_ech_beer / n;
+Freq_ech_spir = Freq_ech_spir / n;
+Freq_ech_vin = Freq_ech_vin / n;
+Freq_ech_pur = Freq_ech_pur / n;
 
-Freq_E_cum_beer = cumsum(Freq_E_beer, 2);
-Freq_E_cum_spir = cumsum(Freq_E_spir, 2);
-Freq_E_cum_wine = cumsum(Freq_E_wine, 2);
-Freq_E_cum_pure = cumsum(Freq_E_pure, 2);
+Freq_ech_cum_beer = cumsum(Freq_ech_beer, 2);
+Freq_ech_cum_spir = cumsum(Freq_ech_spir, 2);
+Freq_ech_cum_vin = cumsum(Freq_ech_vin, 2);
+Freq_ech_cum_pur = cumsum(Freq_ech_pur, 2);
 
-
-
+% Calcul des distances de Kolmogorov Smirnov
 ks_beer = zeros(100, 1);
 ks_spir = zeros(100, 1);
-ks_wine = zeros(100, 1);
-ks_pure = zeros(100, 1);
+ks_vin = zeros(100, 1);
+ks_pur = zeros(100, 1);
 
 for i = 1:100
-    [~,~,ks_beer(i,1)] = kstest2(Freq_cum_beer_popu, Freq_E_cum_beer(i,:));
-    [~,~,ks_spir(i,1)] = kstest2(Freq_cum_spir_popu, Freq_E_cum_spir(i,:));
-    [~,~,ks_wine(i,1)] = kstest2(Freq_cum_wine_popu, Freq_E_cum_wine(i,:));
-    [~,~,ks_pure(i,1)] = kstest2(Freq_cum_pure_popu, Freq_E_cum_pure(i,:));
+    [~,~,ks_beer(i,1)] = kstest2(Freq_cum_beer_pop, Freq_ech_cum_beer(i,:));
+    [~,~,ks_spir(i,1)] = kstest2(Freq_cum_spir_pop, Freq_ech_cum_spir(i,:));
+    [~,~,ks_vin(i,1)] = kstest2(Freq_cum_vin_pop, Freq_ech_cum_vin(i,:));
+    [~,~,ks_pur(i,1)] = kstest2(Freq_cum_pur_pop, Freq_ech_cum_pur(i,:));
 end
 %--------------------------------------------------------------------------
+%% Figures
 
-
+% Histogrammes des distances de Kolmogorov Smirnov
 figure
 histogram(ks_beer);
 set(gca, 'fontsize', 20);
@@ -180,14 +190,14 @@ xlabel('Distance de kolmogorov');
 ylabel('Frequences correspondantes');
 
 figure
-histogram(ks_wine);
+histogram(ks_vin);
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
 xlabel('Distance de kolmogorov');
 ylabel('Frequences correspondantes');
 
 figure
-histogram(ks_pure);
+histogram(ks_pur);
 set(gca, 'fontsize', 20);
 set(gcf,'color','w');
 xlabel('Distance de kolmogorov');
