999dca12ef8abba13ed01e6afadb61a0ee78e07f
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-25 11:15:08 +0100

    Update Q3.m

--- Q3.m
+++ Q3.m
@@ -1,8 +1,8 @@
 %==========================================================================
 % Question 3 : Estimation
 %   
-%   GOFFIN Sven
 %   CRUTZEN Gilles
+%   GOFFIN Sven
 %==========================================================================
 %% Chargement des donnees
 
@@ -20,65 +20,65 @@ Moy_pop = mean(Data(:,3));
 %--------------------------------------------------------------------------
 %% Tirage des echantillons
 
-Vec_ech_20 = zeros(100,n);
+Vect_ech_20 = zeros(100,n);
 
 for i = 1:100
     Ech = datasample(Data,n,1);
-    Vec_ech_20(i,:) = Ech(:,3);
+    Vect_ech_20(i,:) = Ech(:,3);
 end
 %--------------------------------------------------------------------------
 %% 3(a)
 
-Vec_moy_20 = mean(Vec_ech_20, 2);
-Biais_moy_20 = abs(mean(Vec_moy_20) - Moy_pop);
-Var_esti_moy_20 = var(Vec_moy_20);
+Vect_moy_20 = mean(Vect_ech_20, 2);
+Biais_moy_20 = abs(mean(Vect_moy_20) - Moy_pop);
+Var_moy_20 = var(Vect_moy_20);
 %--------------------------------------------------------------------------
 %% 3(b)
 
-Vec_med_20 = median(Vec_ech_20, 2);
-Biais_med_20 = abs(mean(Vec_med_20) - Moy_pop);
-Var_esti_med_20 = var(Vec_med_20);
+Vect_med_20 = median(Vect_ech_20, 2);
+Biais_med_20 = abs(mean(Vect_med_20) - Moy_pop);
+Var_med_20 = var(Vect_med_20);
 %--------------------------------------------------------------------------
 %% Nouvelle taille d'echantillon
 n = 50;
 %--------------------------------------------------------------------------
 %% Tirage des nouveaux echantillons
 
-Vec_ech_50 = zeros(100,n);
+Vect_ech_50 = zeros(100,n);
 
 for i = 1:100
     Ech = datasample(Data,n,1);
-    Vec_ech_50(i,:) = Ech(:,3);
+    Vect_ech_50(i,:) = Ech(:,3);
 end
 %--------------------------------------------------------------------------
 %% 3(c)
 
-Vec_moy_50 = mean(Vec_ech_50, 2);
-Biais_moy_50 = abs(mean(Vec_moy_50) - Moy_pop);
-Var_esti_moy_50 = var(Vec_moy_50);
+Vect_moy_50 = mean(Vect_ech_50, 2);
+Biais_moy_50 = abs(mean(Vect_moy_50) - Moy_pop);
+Var_moy_50 = var(Vect_moy_50);
 
 
-Vec_med_50 = median(Vec_ech_50, 2);
-Biais_med_50 = abs(mean(Vec_med_50) - Moy_pop);
-Var_esti_med_50 = var(Vec_med_50);
+Vect_med_50 = median(Vect_ech_50, 2);
+Biais_med_50 = abs(mean(Vect_med_50) - Moy_pop);
+Var_med_50 = var(Vect_med_50);
 %--------------------------------------------------------------------------
-%% 3(d) donnees utiles
+%% 3(d) Donnees utiles
 n = 20;
 t = 2.093;
 u = 1.96;
-S_19 = std(Vec_ech_20, 0, 2);
+S_19 = std(Vect_ech_20, 0, 2);
 %--------------------------------------------------------------------------
 %% 3(d) i.
 Inter_tstu = zeros(100, 2);
-Inter_tstu(:,1) = Vec_moy_20 - t*S_19/sqrt(n);
-Inter_tstu(:,2) = Vec_moy_20 + t*S_19/sqrt(n);
+Inter_tstu(:,1) = Vect_moy_20 - t*S_19/sqrt(n);
+Inter_tstu(:,2) = Vect_moy_20 + t*S_19/sqrt(n);
 %--------------------------------------------------------------------------
 %% 3(d) ii.
 Inter_Gauss = zeros(100, 2);
-Inter_Gauss(:,1) = Vec_moy_20 - u*S_19/sqrt(n);
-Inter_Gauss(:,2) = Vec_moy_20 + u*S_19/sqrt(n);
+Inter_Gauss(:,1) = Vect_moy_20 - u*S_19/sqrt(n);
+Inter_Gauss(:,2) = Vect_moy_20 + u*S_19/sqrt(n);
 %--------------------------------------------------------------------------
-%% 
+%% Nombre d'IC contenant les valeurs population
 cnt_tstu = 0;
 cnt_Gauss = 0;
 for i = 1:100
@@ -89,3 +89,4 @@ for i = 1:100
         cnt_Gauss = cnt_Gauss + 1;
     end
 end
+%--------------------------------------------------------------------------
