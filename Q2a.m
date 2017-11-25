50ae0a53d36f2e8c2f0599542e64b0cea00cafc6
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-11-25 11:13:58 +0100

    Update Q2a.m

--- Q2a.m
+++ Q2a.m
@@ -1,8 +1,8 @@
 %==========================================================================
-% Question 2 : Generation d'echantillon i.i.d.
+% Question 2 : Generation d'echantillons i.i.d.
 %   
-%   GOFFIN Sven
 %   CRUTZEN Gilles
+%   GOFFIN Sven
 %==========================================================================
 %% Chargement des donnees
 
@@ -18,50 +18,50 @@ end
 %--------------------------------------------------------------------------
 %% 2(a) Tirage de l'echantillon
 
-E = datasample(Data, n, 1);
+Ech = datasample(Data, n, 1);
 %--------------------------------------------------------------------------
-%% 2(a)i. Moyenne, mediane et ecart-type de l'echantillon (biere et spiritueux)
+%% 2(a)i. Moyenne, mediane et ecart-type echantillon (biere et spiritueux)
 
-Moy_E_beer = mean(E(:,1));
-Moy_E_spir = mean(E(:,2));
+Moy_Ech_beer = mean(Ech(:,1));
+Moy_Ech_spir = mean(Ech(:,2));
 
-Med_E_beer = median(E(:,1));
-Med_E_spir = median(E(:,2));
+Med_Ech_beer = median(Ech(:,1));
+Med_Ech_spir = median(Ech(:,2));
 
-ET_E_beer = std(E(:,1));
-ET_E_spir = std(E(:,2));
+ET_Ech_beer = std(Ech(:,1));
+ET_Ech_spir = std(Ech(:,2));
 %--------------------------------------------------------------------------
-%% 2(a)ii. Boites a moustaches (consommation de biere et de spiritueux)
+%% 2(a)ii. Boites a moustaches (consommations de biere et de spiritueux)
 
 E_red = zeros(n,2);
-E_red(:,1) = E(:,1);
-E_red(:,2) = E(:,2);
+E_red(:,1) = Ech(:,1);
+E_red(:,2) = Ech(:,2);
 
 boxplot(E_red);
 %--------------------------------------------------------------------------
-%% 2(a)iii. Polygones des frequences cumulees de la consommation de biere et de spiritueux
+%% 2(a)iii. Polygones des frequences cumulees (biere et spiritueux)
 
-Cons_E_beer = 0:max(Data(:,1));
-Cons_E_spir = 0:max(Data(:,2));
+Cons_ech_beer = 0:max(Data(:,1));
+Cons_ech_spir = 0:max(Data(:,2));
 
-Freq_E_beer = zeros(1, max(Data(:,1)) + 1);
-Freq_E_spir = zeros(1, max(Data(:,2)) + 1);
+Freq_ech_beer = zeros(1, max(Data(:,1)) + 1);
+Freq_ech_spir = zeros(1, max(Data(:,2)) + 1);
 
 for i = 1:n
-    Freq_E_beer(1, E(i, 1) + 1) = Freq_E_beer(1, E(i, 1) + 1) + 1;
-    Freq_E_spir(1, E(i, 2) + 1) = Freq_E_spir(1, E(i, 2) + 1) + 1;
+    Freq_ech_beer(1, Ech(i, 1) + 1) = Freq_ech_beer(1, Ech(i, 1) + 1) + 1;
+    Freq_ech_spir(1, Ech(i, 2) + 1) = Freq_ech_spir(1, Ech(i, 2) + 1) + 1;
 end
-figure;
-Freq_E_beer = Freq_E_beer / n;
-Freq_E_spir = Freq_E_spir / n;
-Freq_E_cum_beer = cumsum(Freq_E_beer);
-Freq_E_cum_spir = cumsum(Freq_E_spir);
+
+Freq_ech_beer = Freq_ech_beer / n;
+Freq_ech_spir = Freq_ech_spir / n;
+Freq_E_cum_beer = cumsum(Freq_ech_beer);
+Freq_E_cum_spir = cumsum(Freq_ech_spir);
 
 figure;
-plot(Cons_E_beer, Freq_E_cum_beer);
+plot(Cons_ech_beer, Freq_E_cum_beer);
 title('Polygone des frequences cumulees de la consommation de biere');
 figure;
-plot(Cons_E_spir, Freq_E_cum_spir);
+plot(Cons_ech_spir, Freq_E_cum_spir);
 title('Polygone des frequences cumulees de la consommation de spiritueux');
 
 % Calcul des distances de Kolmogorov Smirnov
